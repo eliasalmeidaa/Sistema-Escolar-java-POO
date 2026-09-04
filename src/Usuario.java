afe05d00a11e1138cd9a10132e5c76574283f5c8
eliasalmeidaa <eliasmartinsdealmeida@gmail.com>
2026-09-03 22:23:03 -0300

    Colocado as informações centrais do professor e do aluno

---
+++ src/Usuario.java
@@ -0,0 +1,36 @@
+public class Usuario {
+
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Usuario(String nome, String cpf, String email){
+        this.setNome(nome);
+        this.setCpf(cpf);
+        this.setEmail(email);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
